bc460162112358f67eba806b640634726018e593
luizdembicki <luiz.dembicki@ymail.com>
2023-10-28 01:36:39 -0300

    add logos

---
+++ .history/mod_20231028011736.cpp
@@ -0,0 +1,14 @@
+name = "BRAF Halloween";       // Name of your mod
+author = "BRAF Team & Ludwig"; // Affects Arma 3 Launcher, when the mod are loaded as local
+logo = "logo_co.paa";          // Logo displayed in the main menu
+logoOver = "logo_hover_co.pa"; // When the mouse is over, in the main menu
+tooltip = "mod_";
+tooltipOwned = "credit_mod";    // Tool tip displayed when the mouse is left over, in the main menu
+picture = "files\overview.paa"; // Picture displayed from the expansions menu. Optimal size is 2048x1024
+actionName = "GitHub";
+action = "https://github.com/my-mod-page";         // Website URL, that can be accessed from the expansions menu
+overview = "This awesome mod does this and that."; // Supports structured text
+hideName = 0;                                      // Hide the extension name
+hidePicture = 0;                                   // Hide the extension menu
+dlcColor[] = {0.23, 0.39, 0.30, 1};                // Color used for DLC stripes and backgrounds (RGBA)
+logoSmall = "files\ic_small.paa";                  // Display in creative lists, next to the entities added by the mod
---
+++ .history/mod_20231028011921.cpp
@@ -0,0 +1,10 @@
+name = "BRAF Halloween";        // Name of your mod
+author = "BRAF Team & Ludwig";  // Affects Arma 3 Launcher, when the mod are loaded as local
+logo = "logo_co.paa";           // Logo displayed in the main menu
+logoOver = "logo_hover_co.paa"; // When the mouse is over, in the main menu
+tooltip = "BRAF Halloween";
+tooltipOwned = "BRAF Team & Ludwig"; // Tool tip displayed when the mouse is left over, in the main menu
+actionName = "BRAF Mod";
+action = "https://brafmod.com.br";       // Website URL, that can be accessed from the expansions menu
+overview = "BRAF Mod Halloween extras."; // Supports structured text
+logoSmall = "files\ic_small.paa";        // Display in creative lists, next to the entities added by the mod
